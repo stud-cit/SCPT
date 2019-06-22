47cab97790f791f484123fe5b1f2abefa87116d1
Helsis <seagull.bogdan@gmail.com>
2019-06-22 15:25:03 +0300

    refactor ( Carousel ) : Old slider

---
+++ client/components/Carousel.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-carousel height="100vh" hide-controls hide-delimiters>
+    <v-carousel-item v-for="(item, i) in items" :key="i" :src="item" />
+  </v-carousel>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component()
+export default class Carousel extends Vue {
+  @Prop({ type: Array, required: true }) items: [String];
+}
+</script>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app light>
     <Toolbar :pages="pages" />
-    <Slider />
+    <Carousel :items="carusel" />
     <BottomNavigation :pages="pages" />
     <v-content>
       <v-layout row wrap>
         <v-flex xs12 md3>
-          <Sidebar :items="items" />
+          <Sidebar :items="directions" />
         </v-flex>
         <v-flex xs12 md9>
           <nuxt />
@@ -24,118 +24,121 @@ import { Component, Vue } from 'vue-property-decorator';
     Toolbar: () => import('~/components/Toolbar'),
     BottomNavigation: () => import('~/components/BottomNavigation'),
     Footer: () => import('~/components/Footer'),
-    Slider: () => import('~/components/Slider.vue'),
+    Carousel: () => import('~/components/Carousel.vue'),
     Sidebar: () => import('~/components/Sidebar.vue'),
   },
-  data: () => ({
-    pages: [
-      { title: 'Головна', icon: 'mdi-home', to: '/' },
-      { title: 'Документи', icon: 'mdi-file-document', to: '/docs' },
-      { title: 'Галерея', icon: 'mdi-animation', to: '/gallery' },
-      { title: 'Контакти', icon: 'mdi-contacts', to: '/contact' },
-      { title: 'Про нас', icon: 'mdi-information-variant', to: '/about' },
-    ],
-    invites: [
-      { icon: 'mdi-instagram', to: '//google.com.ua' },
-      { icon: 'mdi-facebook', to: '//google.com.ru' },
-      { icon: 'mdi-linkedin', to: '//google.com.sd' },
-      { icon: 'mdi-twitter', to: '//google.com' },
-    ],
-    items: [
-      {
-        icon: 'mdi-heart',
-        title:
-          "Організація життєзабезпечення об'єднаних територіальних громад, селищних та сільских рад",
-        items: [
-          { icon: 'mdi-file-document', title: 'Документація' },
-          { icon: 'mdi-presentation', title: 'Презентація' },
-          { icon: 'mdi-information-variant', title: 'Загальна інформація' },
-        ],
-      },
-      {
-        icon: 'mdi-flower-poppy',
-        title: 'Основи садівництва',
-        items: [
-          { icon: 'mdi-file-document', title: 'Документація' },
-          { icon: 'mdi-presentation', title: 'Презентація' },
-          { icon: 'mdi-information-variant', title: 'Загальна інформація' },
-        ],
-      },
-      {
-        icon: 'mdi-inbox-multiple',
-        title: 'Основи бджільництва',
-        items: [
-          { icon: 'mdi-file-document', title: 'Документація' },
-          { icon: 'mdi-presentation', title: 'Презентація' },
-          { icon: 'mdi-information-variant', title: 'Загальна інформація' },
-        ],
-      },
-      {
-        icon: 'mdi-pine-tree',
-        title:
-          'Благоустрій та озелення території з елементами ландшафтного дизайну',
-        items: [
-          { icon: 'mdi-file-document', title: 'Документація' },
-          { icon: 'mdi-presentation', title: 'Презентація' },
-          { icon: 'mdi-information-variant', title: 'Загальна інформація' },
-        ],
-      },
-      {
-        icon: 'mdi-store',
-        title: 'Основи малого підприємництва',
-        items: [
-          { icon: 'mdi-file-document', title: 'Документація' },
-          { icon: 'mdi-presentation', title: 'Презентація' },
-          { icon: 'mdi-information-variant', title: 'Загальна інформація' },
-        ],
-      },
-      {
-        icon: 'mdi-laptop',
-        title: "Комп'ютерні теехнології обробки бухгалтерської документації",
-        items: [
-          { icon: 'mdi-file-document', title: 'Документація' },
-          { icon: 'mdi-presentation', title: 'Презентація' },
-          { icon: 'mdi-information-variant', title: 'Загальна інформація' },
-        ],
-      },
-      {
-        icon: 'mdi-domain',
-        title: 'Основи службового етикету та сучасне діловодство',
-        items: [
-          { icon: 'mdi-file-document', title: 'Документація' },
-          { icon: 'mdi-presentation', title: 'Презентація' },
-          { icon: 'mdi-information-variant', title: 'Загальна інформація' },
-        ],
-      },
-      {
-        icon: 'mdi-creation',
-        title: 'Основи перукарської справи',
-        items: [
-          { icon: 'mdi-file-document', title: 'Документація' },
-          { icon: 'mdi-presentation', title: 'Презентація' },
-          { icon: 'mdi-information-variant', title: 'Загальна інформація' },
-        ],
-      },
-      {
-        icon: 'mdi-shoe-print',
-        title: 'Технологія ремонту взуття та одягу',
-        items: [
-          { icon: 'mdi-file-document', title: 'Документація' },
-          { icon: 'mdi-presentation', title: 'Презентація' },
-          { icon: 'mdi-information-variant', title: 'Загальна інформація' },
-        ],
-      },
-      {
-        icon: 'mdi-brush',
-        title: 'Художне оздоблення одягу у швейному виробництві',
-        items: [
-          { icon: 'mdi-file-document', title: 'Документація' },
-          { icon: 'mdi-presentation', title: 'Презентація' },
-          { icon: 'mdi-information-variant', title: 'Загальна інформація' },
-        ],
-      },
-    ],
-  }),
 })
-export default class DefaultLayuot extends Vue {}
+export default class DefaultLayuot extends Vue {
+  pages = [
+    { title: 'Головна', icon: 'mdi-home', to: '/' },
+    { title: 'Документи', icon: 'mdi-file-document', to: '/docs' },
+    { title: 'Галерея', icon: 'mdi-animation', to: '/gallery' },
+    { title: 'Контакти', icon: 'mdi-contacts', to: '/contact' },
+    { title: 'Про нас', icon: 'mdi-information-variant', to: '/about' },
+  ];
+
+  invites = [
+    { icon: 'mdi-instagram', to: '//google.com.ua' },
+    { icon: 'mdi-facebook', to: '//google.com.ru' },
+    { icon: 'mdi-linkedin', to: '//google.com.sd' },
+    { icon: 'mdi-twitter', to: '//google.com' },
+  ];
+
+  carusel = new Array(5).fill(`./img/placeholder.svg`);
+
+  directions = [
+    {
+      icon: 'mdi-heart',
+      title:
+        "Організація життєзабезпечення об'єднаних територіальних громад, селищних та сільских рад",
+      items: [
+        { icon: 'mdi-file-document', title: 'Документація' },
+        { icon: 'mdi-presentation', title: 'Презентація' },
+        { icon: 'mdi-information-variant', title: 'Загальна інформація' },
+      ],
+    },
+    {
+      icon: 'mdi-flower-poppy',
+      title: 'Основи садівництва',
+      items: [
+        { icon: 'mdi-file-document', title: 'Документація' },
+        { icon: 'mdi-presentation', title: 'Презентація' },
+        { icon: 'mdi-information-variant', title: 'Загальна інформація' },
+      ],
+    },
+    {
+      icon: 'mdi-inbox-multiple',
+      title: 'Основи бджільництва',
+      items: [
+        { icon: 'mdi-file-document', title: 'Документація' },
+        { icon: 'mdi-presentation', title: 'Презентація' },
+        { icon: 'mdi-information-variant', title: 'Загальна інформація' },
+      ],
+    },
+    {
+      icon: 'mdi-pine-tree',
+      title:
+        'Благоустрій та озелення території з елементами ландшафтного дизайну',
+      items: [
+        { icon: 'mdi-file-document', title: 'Документація' },
+        { icon: 'mdi-presentation', title: 'Презентація' },
+        { icon: 'mdi-information-variant', title: 'Загальна інформація' },
+      ],
+    },
+    {
+      icon: 'mdi-store',
+      title: 'Основи малого підприємництва',
+      items: [
+        { icon: 'mdi-file-document', title: 'Документація' },
+        { icon: 'mdi-presentation', title: 'Презентація' },
+        { icon: 'mdi-information-variant', title: 'Загальна інформація' },
+      ],
+    },
+    {
+      icon: 'mdi-laptop',
+      title: "Комп'ютерні теехнології обробки бухгалтерської документації",
+      items: [
+        { icon: 'mdi-file-document', title: 'Документація' },
+        { icon: 'mdi-presentation', title: 'Презентація' },
+        { icon: 'mdi-information-variant', title: 'Загальна інформація' },
+      ],
+    },
+    {
+      icon: 'mdi-domain',
+      title: 'Основи службового етикету та сучасне діловодство',
+      items: [
+        { icon: 'mdi-file-document', title: 'Документація' },
+        { icon: 'mdi-presentation', title: 'Презентація' },
+        { icon: 'mdi-information-variant', title: 'Загальна інформація' },
+      ],
+    },
+    {
+      icon: 'mdi-creation',
+      title: 'Основи перукарської справи',
+      items: [
+        { icon: 'mdi-file-document', title: 'Документація' },
+        { icon: 'mdi-presentation', title: 'Презентація' },
+        { icon: 'mdi-information-variant', title: 'Загальна інформація' },
+      ],
+    },
+    {
+      icon: 'mdi-shoe-print',
+      title: 'Технологія ремонту взуття та одягу',
+      items: [
+        { icon: 'mdi-file-document', title: 'Документація' },
+        { icon: 'mdi-presentation', title: 'Презентація' },
+        { icon: 'mdi-information-variant', title: 'Загальна інформація' },
+      ],
+    },
+    {
+      icon: 'mdi-brush',
+      title: 'Художне оздоблення одягу у швейному виробництві',
+      items: [
+        { icon: 'mdi-file-document', title: 'Документація' },
+        { icon: 'mdi-presentation', title: 'Презентація' },
+        { icon: 'mdi-information-variant', title: 'Загальна інформація' },
+      ],
+    },
+  ];
+}
 </script>
