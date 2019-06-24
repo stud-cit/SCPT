b066d75025c88beba9540769bda6106caf424380
Helsis <seagull.bogdan@gmail.com>
2019-06-24 17:27:06 +0300

    fix ( Carousel ): change behavior relative to page path

--- client/components/Carousel.vue
+++ client/components/Carousel.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-carousel height="100vh" hide-controls hide-delimiters>
+  <v-carousel :height="onChangeHeight()" hide-controls hide-delimiters>
     <v-carousel-item v-for="(item, i) in items" :key="i" :src="item" />
   </v-carousel>
 </template>
@@ -10,5 +10,13 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 @Component()
 export default class Carousel extends Vue {
   @Prop({ type: Array, required: true }) items: [String];
+
+  onChangeHeight () {
+    if(this.$route.path === '/') {
+      return '100vh'
+    }
+
+    return '70vh'
+  }
 }
 </script>
