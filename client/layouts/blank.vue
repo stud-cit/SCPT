f32c49d4d2e7e2e76f76784e515c3acc1e3e7a79
Helsis <seagull.bogdan@gmail.com>
2019-06-24 22:25:43 +0300

    feet( auth page )
    Add sign-in / sign-up form 
    Delete login page

---
+++ client/layouts/blank.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-app>
+    <v-content>
+      <nuxt />
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component()
+export default class BlankLayuot extends Vue {}
+</script>
