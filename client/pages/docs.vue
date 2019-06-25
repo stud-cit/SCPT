c2e26ee51d9a4fcf72196213f1d1628ddd84e87c
Helsis <seagull.bogdan@gmail.com>
2019-06-25 06:56:33 +0300

    chore: formatting

--- client/pages/docs.vue
+++ client/pages/docs.vue
@@ -1,10 +1,9 @@
 <template>
   <v-container grid-list-lg>
     <v-layout align-center justify-space-around wrap>
-
       <v-flex v-for="(doc, i) in documents" :key="i" xs12 sm6 md4 lg3>
         <v-card>
-          <v-img class="white--text" :src="doc.src" :aspect-ratio="70/99"/>
+          <v-img class="white--text" :src="doc.src" :aspect-ratio="70 / 99" />
           <v-card-actions class="pt-4" style="position: relative; z-index:0">
             <v-btn
               @click="download(doc.to)"
@@ -21,7 +20,6 @@
           </v-card-actions>
         </v-card>
       </v-flex>
-
     </v-layout>
   </v-container>
 </template>
@@ -32,15 +30,17 @@ import { Component, Vue } from 'vue-property-decorator';
 @Component({
   head: {
     title: 'Документи',
-  }
+  },
 })
 export default class DocsPage extends Vue {
   documents = new Array(13).fill('').map((item, i) => ({
-    title: `Документ ${i}`, src: './img/placeholder.svg', to: '/'
-  }))
+    title: `Документ ${i}`,
+    src: './img/placeholder.svg',
+    to: '/',
+  }));
 
-  async download(src){
-    return await 0
+  async download(src) {
+    return await 0;
   }
 }
 </script>
@@ -50,9 +50,8 @@ export default class DocsPage extends Vue {
   align-items: center;
   bottom: 0;
   justify-content: center;
-  opacity: .5;
+  opacity: 0.5;
   position: absolute;
   width: 100%;
 }
-
 </style>
