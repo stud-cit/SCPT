5e13f49e5c5e1369474a878c5538effc32cb8a98
Helsis <seagull.bogdan@gmail.com>
2019-06-25 23:51:44 +0300

    feet ( add image custom component )

---
+++ client/components/CustomImage.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-img
+    :src="data.src"
+    :lazy-src="data.lazySrc"
+    :alt="data.alt"
+    v-bind="$attrs"
+  >
+    <slot />
+    <template v-slot:placeholder>
+      <v-layout fill-height align-center justify-center ma-0>
+        <v-progress-circular :size="50" color="accent" indeterminate />
+      </v-layout>
+    </template>
+  </v-img>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component()
+export default class CustomImage extends Vue {
+  @Prop({ type: Object, required: true }) data: any;
+}
+</script>
+
+<style>
+.card--flex-toolbar {
+  margin-top: -64px;
+}
+</style>
