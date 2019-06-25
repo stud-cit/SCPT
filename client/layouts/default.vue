4d04604e0221e3e09584ca5ca586ee4abc12bd6d
Helsis <seagull.bogdan@gmail.com>
2019-06-25 04:59:34 +0300

    refactoring ( clear components )

--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,12 +1,70 @@
 <template>
   <v-app light>
-    <Toolbar :pages="pages" />
-    <Carousel :items="carusel" />
-    <BottomNavigation :pages="pages" />
+    <v-toolbar app fixed class="white--text hidden-xs-only" color="indigo">
+      <v-toolbar-title>
+        <v-icon color="white">mdi-vuetify</v-icon>
+        Title
+      </v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-items v-for="(page, i) in pages" :key="i">
+        <v-btn flat :to="page.to" exact>
+          <div class=" white--text">{{ page.title }}</div>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+
+    <Carousel :items="carusel" hide-controls hide-delimiters />
+
     <v-content>
       <nuxt />
     </v-content>
-    <Footer :invites="invites" />
+
+    <v-footer height="auto">
+      <v-card color="indigo" flat tile class="text-xs-center">
+        <v-card-text>
+          <v-btn
+            icon
+            class="mx-3 white--text"
+            v-for="invite in invites"
+            :key="invite.to"
+            :to="invite.to"
+          >
+            <v-icon v-text="invite.icon" />
+          </v-btn>
+        </v-card-text>
+
+        <v-card-text>
+          <v-layout wrap align-center justify-space-around>
+            <v-flex md5 sm12 class="white--text">
+              Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit
+              amet. Mauris cursus commodo interdum. Praesent ut risus eget metus
+              luctus accumsan id ultrices nunc. Sed at orci sed massa
+              consectetur dignissim a sit amet dui. Duis commodo vitae velit et
+              faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue
+              vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu
+              ipsum vel diam elementum tempor vel ut orci. Orci varius natoque
+              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            </v-flex>
+            <v-flex md5 sm12 class="white--text">
+              Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit
+              amet. Mauris cursus commodo interdum. Praesent ut risus eget metus
+              luctus accumsan id ultrices nunc. Sed at orci sed massa
+              consectetur dignissim a sit amet dui. Duis commodo vitae velit et
+              faucibus. Morbi vehicula lacinia malesuada. Nulla placerat augue
+              vel ipsum ultrices, cursus iaculis dui sollicitudin. Vestibulum eu
+              ipsum vel diam elementum tempor vel ut orci. Orci varius natoque
+              penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            </v-flex>
+          </v-layout>
+        </v-card-text>
+
+        <v-divider />
+
+        <v-card-text class="white--text">
+          &copy;2019 — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
   </v-app>
 </template>
 <script lang="ts">
@@ -14,11 +72,7 @@ import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
   components: {
-    Toolbar: () => import('~/components/Toolbar'),
-    BottomNavigation: () => import('~/components/BottomNavigation'),
-    Footer: () => import('~/components/Footer'),
     Carousel: () => import('~/components/Carousel.vue'),
-    Sidebar: () => import('~/components/Sidebar.vue'),
   },
 })
 export default class DefaultLayuot extends Vue {
@@ -37,51 +91,6 @@ export default class DefaultLayuot extends Vue {
     { icon: 'mdi-twitter', to: '//google.com' },
   ];
 
-  directions = [
-    {
-      icon: 'mdi-heart',
-      title:
-        "Організація життєзабезпечення об'єднаних територіальних громад, селищних та сільских рад",
-    },
-    {
-      icon: 'mdi-flower-poppy',
-      title: 'Основи садівництва',
-    },
-    {
-      icon: 'mdi-inbox-multiple',
-      title: 'Основи бджільництва',
-    },
-    {
-      icon: 'mdi-pine-tree',
-      title:
-        'Благоустрій та озелення території з елементами ландшафтного дизайну',
-    },
-    {
-      icon: 'mdi-store',
-      title: 'Основи малого підприємництва',
-    },
-    {
-      icon: 'mdi-laptop',
-      title: "Комп'ютерні теехнології обробки бухгалтерської документації",
-    },
-    {
-      icon: 'mdi-domain',
-      title: 'Основи службового етикету та сучасне діловодство',
-    },
-    {
-      icon: 'mdi-creation',
-      title: 'Основи перукарської справи',
-    },
-    {
-      icon: 'mdi-shoe-print',
-      title: 'Технологія ремонту взуття та одягу',
-    },
-    {
-      icon: 'mdi-brush',
-      title: 'Художне оздоблення одягу у швейному виробництві',
-    },
-  ];
-
-  carusel = new Array(5).fill(`./img/placeholder.svg`);
+  carusel = new Array(5).fill({ src: './img/placeholder.svg' });
 }
 </script>
