10c8def8ad3a85d9ef97192426f23b30b6766d55
Helsis <seagull.bogdan@gmail.com>
2019-06-27 15:21:08 +0300

    feet ( dialog backgroun ): change to image

--- client/components/ArticleViewer.vue
+++ client/components/ArticleViewer.vue
@@ -8,13 +8,14 @@
       transition="dialog-bottom-transition"
     >
       <v-card flat>
-        <v-toolbar color="primary" dark extended flat prominent>
-          <v-btn icon @click.native="$emit('input')">
+        <v-toolbar dark flat color="primary" v-bind:style="setBackground(data)">
+          <v-spacer />
+          <v-btn fab flat @click.native="$emit('input')">
             <v-icon>mdi-close</v-icon>
           </v-btn>
         </v-toolbar>
-        <v-layout row>
-          <v-flex xs8 offset-xs2>
+        <v-layout align-center justify-center row fill-height>
+          <v-flex xs12 sm10 md8 lg6>
             <v-card class="card--flex-toolbar">
               <v-toolbar card prominent class="py-2">
                 <v-toolbar-title class="grey--text title">
@@ -30,9 +31,9 @@
 
               <v-divider></v-divider>
 
-              <v-card-actions class="primary white--text">
+              <v-card-actions>
                 <v-spacer />
-                <v-icon class="mr-2 white--text" small>mdi-update</v-icon>
+                <v-icon class="mr-2" small>mdi-update</v-icon>
                 <span>{{ toFormatDate(data.updateAt) }}</span>
               </v-card-actions>
             </v-card>
@@ -50,6 +51,15 @@ import { Component, Vue, Prop } from 'vue-property-decorator';
 export default class ArticleViewer extends Vue {
   @Prop({ type: Boolean, required: false }) value: any;
   @Prop({ type: Object, required: true }) data: any;
+
+  setBackground(data) {
+    return {
+      height: '25vh',
+      'background-image': `url(${data.src})`,
+      'background-size': 'cover',
+      'background-position-y': 'center',
+    };
+  }
 }
 </script>
 
