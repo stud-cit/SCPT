05a41541ef3510ce0df2f8a68712419940f1f9da
ZenEnd <o.s.maksimenko@gmail.com>
2019-06-29 10:40:16 +0300

    feet ( add scroll to directions )

--- client/pages/directions.vue
+++ client/pages/directions.vue
@@ -1,12 +1,18 @@
 <template>
   <v-layout column>
-    <v-container v-for="(way, i) in ways" :key="i">
-      <v-layout class="display-1 font-weight-light my-3">
-        {{ way.title }}
+    <v-flex xs5 row>
+      <v-layout>
+        <v-select v-model="id" :items="ids" label="Напрямки"></v-select>
+        <v-btn ref="button" color="primary" flat block @click="$vuetify.goTo(target(),{durraton:300})">Обрати</v-btn>
+    </v-layout>
+    </v-flex>
+    <v-container v-for="(dirrection, i) in dirrections" :key="i">
+      <v-layout :id="dirrection.id"class="display-1 font-weight-light my-3">
+        {{ dirrection.title }}
       </v-layout>
       <v-container fluid grid-list-lg>
         <v-layout class="box-img" align-center justify-center row wrap>
-          <v-flex v-for="(doc, i) in way.documents" :key="i" xs4>
+          <v-flex v-for="(doc, i) in dirrection.documents" :key="i" xs4>
             <v-card class="doc">
               <CustomImage
                 class="doc-image"
@@ -32,10 +38,10 @@
             </v-card>
           </v-flex>
           <v-flex xs8>
-            <Carousel :items="carusel" hide-delimiters />
+            <Carousel :cycle="false" :items="images" hide-delimiters />
           </v-flex>
           <v-flex>
-            <span class="mb-2">{{ way.content }}</span>
+            <span class="mb-2">{{ dirrection.content }}</span>
           </v-flex>
         </v-layout>
       </v-container>
@@ -56,8 +62,13 @@ import { Component, Vue } from 'vue-property-decorator';
   },
 })
 export default class WayPage extends Vue {
-  ways = new Array(10).fill('').map((item, i) => ({
+  id = "#direction1";
+  ids = new Array(10).fill('').map((item, i) => ([
+      `#direction${i+1}`
+  ]));
+  dirrections = new Array(10).fill('').map((item, i) => ({
     title: `Напрямок ${i + 1}`,
+    id: `direction${i+1}`,
     documents: [
       {
         title: `Документ ${i + 1}`,
@@ -66,18 +77,18 @@ export default class WayPage extends Vue {
         to: '/',
       },
     ],
-    carusel: new Array(5).fill('').map((item, i) => ({
-      src: `https://picsum.photos/1366/728?image=${i * 5 + 10}`,
-    })),
     content:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
   }));
-  carusel = new Array(5).fill('').map((item, i) => ({
+  images = new Array(5).fill('').map((item, i) => ({
     src: `https://picsum.photos/1366/728?image=${i * 5 + 10}`,
   }));
 
   async download(src) {
     return await 0;
+  }
+  target() {
+    return this.id[0];
   }
 }
 </script>
