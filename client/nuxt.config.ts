b6e3b659de024da36c7128e56cbfde6422a74566
Helsis <seagull.bogdan@gmail.com>
2019-07-01 01:38:07 +0300

    feat ( nuxt configuration for auth flow )

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -4,6 +4,11 @@ import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin';
 import { ConfigService } from './utils/config/config.service';
 const configService = new ConfigService();
 
+console.log([
+  configService.getSetting('APP_PORT'),
+  configService.getSetting('APP_TIMG'),
+]);
+
 const config: NuxtConfiguration = {
   /*
    ** Server options
@@ -47,25 +52,6 @@ const config: NuxtConfiguration = {
   /*
    * Build configuration
    */
-
-  modules: ['@nuxtjs/axios', '@nuxtjs/auth'],
-
-  axios: {
-    baseURL: 'http://127.0.0.1:8081/api',
-  },
-
-  auth: {
-    strategies: {
-      local: {
-        endpoints: {
-          login: { url: '/auth', method: 'post', propertyName: 'token' },
-          user: { url: '/users', method: 'get', propertyName: '' },
-          logout: false,
-        },
-      },
-    },
-  },
-
   build: {
     extractCSS: true,
     transpile: ['vuetify/lib'],
