bcb6f78e9bfd0bec08eb78849b7378fab5698ca5
ZenEnd <o.s.maksimenko@gmail.com>
2019-07-05 14:58:47 +0300

    feat ( add carousel change )

--- client/pages/admin/carousel.vue
+++ client/pages/admin/carousel.vue
@@ -1,13 +1,35 @@
 <template>
-  <v-container>
-    <v-layout align-cente justify-center wrap>
-      <v-flex xs12>
-        <CustomImage :data="items[page - 1]" />
-        <v-text-field v-model="items[page - 1].src" label="src" />
-        <v-text-field v-model="items[page - 1].lazySrc" label="lazySrc" />
+  <v-container grid-list-lg>
+    <v-layout wrap>
+      <v-flex xs12 sm5 md4 lg2 py-3>
+        <v-card>
+          <v-responsive :aspect-ratio="70 / 99">
+            <v-layout align-center justify-center fill-height>
+              <v-btn @click="$refs.file.click()" outline fab large>
+                <v-icon large>mdi-plus</v-icon>
+              </v-btn>
+            </v-layout>
+          </v-responsive>
+        </v-card>
       </v-flex>
-      <v-pagination v-model="page" :length="itemsLength(items)"> </v-pagination>
+
+      <v-flex v-for="(src, i) in carousel" :key="i" xs12 sm5 md4 lg2 py-3>
+        <v-card>
+          <CustomImage :data="src" :aspect-ratio="70 / 99" />
+            <v-btn @click="deleteImage(i)" color="primary" absolute right bottom fab>
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+        </v-card>
+      </v-flex>
     </v-layout>
+
+    <input
+      v-show
+      ref="file"
+      type="file"
+      accept="image/*"
+      @change="onFileChange"
+    />
   </v-container>
 </template>
 
@@ -24,15 +46,29 @@ import { Component, Vue } from 'vue-property-decorator';
   },
 })
 export default class AdminPage extends Vue {
-  page: number = 1;
-  items = new Array(15).fill('').map((item, i) => ({
-    type: 'video',
-    title: `Alt item ${i}`,
+  dialog: boolean = false;
+  currentItem = null;
+  carousel = new Array(5).fill('').map((item, i) => ({
     src: `https://picsum.photos/1366/728?image=${i * 5 + 10}`,
-    lazySrc: `https://picsum.photos/10/6?image=${i * 5 + 10}`,
   }));
-  itemsLength(items) {
-    return items.length;
+
+  async onFileChange(e) {
+    const file = e.target.files[0];
+    const data = new FormData();
+    data.append('file', file);
+    const patch = await this.$axios.$post(`upload/`, data, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    });
+
+    return this.carousel.push({
+      src: src
+    })
+  }
+
+  deleteImage(i) {
+    this.carousel.splice(i, 1)
   }
 }
 </script>
